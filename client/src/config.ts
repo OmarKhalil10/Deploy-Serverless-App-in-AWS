e36bf70466c4fd8db9710adb6330bbda0c70a737
Omar Khalil <omarkhalil770@gmail.com>
2022-10-14 07:40:08 +0200

    initial commit

--- client/src/config.ts
+++ client/src/config.ts
@@ -5,7 +5,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-qt-sosv5.us.auth0.com',            // Auth0 domain
+  clientId: 'SEs1Hyo0ZDhYyUdVFP7a6Mt1DnrtqcAw',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
