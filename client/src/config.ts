c4c69341622b637dbc38756ac1ad1df674153bd1
Omar Khalil <omarkhalil770@gmail.com>
2022-10-14 20:53:21 +0200

    update the client end point

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,9 +1,9 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '4w83v306c9'
+const apiId = 'fau88y9u92'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/prod`
 export const authConfig = {
 
-  // Service Endpoint: https://4w83v306c9.execute-api.us-east-1.amazonaws.com/prod
+  // https://fau88y9u92.execute-api.us-east-1.amazonaws.com/prod
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-qt-sosv5.us.auth0.com',            // Auth0 domain
