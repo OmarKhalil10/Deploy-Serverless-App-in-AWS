301827fc6f25235f6f36d1135e0b0b773940501b
Omar Khalil <omarkhalil770@gmail.com>
2022-10-14 09:23:47 +0200

    completed the project

--- backend/src/requests/CreateTodoRequest.ts
+++ backend/src/requests/CreateTodoRequest.ts
@@ -4,5 +4,4 @@
 export interface CreateTodoRequest {
   name: string
   dueDate: string
-  done: boolean
 }
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,9 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
-
+const apiId = '4w83v306c9'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/prod`
 export const authConfig = {
+
+  // Service Endpoint: https://4w83v306c9.execute-api.us-east-1.amazonaws.com/prod
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-qt-sosv5.us.auth0.com',            // Auth0 domain
