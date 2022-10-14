bd8c3454e4345bcb60e75f3caeb0c947e598780a
Omar Khalil <omarkhalil770@gmail.com>
2022-10-14 23:06:34 +0200

    fix auth0Autorizer file

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,9 +1,9 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'fau88y9u92'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/prod`
+const apiId = 'o7c6hu2xnc'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 export const authConfig = {
 
-  // https://fau88y9u92.execute-api.us-east-1.amazonaws.com/prod
+  // https://o7c6hu2xnc.execute-api.us-east-1.amazonaws.com/dev
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-qt-sosv5.us.auth0.com',            // Auth0 domain
